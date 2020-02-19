e9ab52baeb12a3b61aa29a642cb8648d46b0c451
hp <ayush.singhal_cs18@gla.ac.in>
2020-02-19 16:44:19 +0530

    some development in the person class and basic panel makin g in main class

---
+++ src/contactslist/Main.java
@@ -0,0 +1,18 @@
+package contactslist;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to ayush's contact list");
+        System.out.println("press 1 to add a contact");
+        System.out.println("press 2 to add a contact");
+        System.out.println("press 3 to search for a contact");
+        System.out.println("press 4 to delete a contact");
+        System.out.println("press 5 to exit my contact list");
+        Scanner sc = new Scanner(System.in);
+        int options = sc.nextInt();
+
+
+    }
+}
--- src/contactslist/Person.java
+++ src/contactslist/Person.java
@@ -1,5 +1,49 @@
 package contactslist;
 
+import java.util.*;
+
 public class Person {
-    String fir;
+    String first_name;
+    String last_name;
+    List<Long> contact_list = new ArrayList<>();
+    String email;
+
+    public Person(String first_name, String last_name, List<Long> contactlist, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.contact_list = contactlist;
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public List<Long> getContact_list() {
+        return contact_list;
+    }
+
+    public void setContact_list(List<Long> contact_list) {
+        this.contact_list = contact_list;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
